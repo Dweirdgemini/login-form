0dfd3355fed2130f08ff588df8daa2f76843cfec
DEV NG <nwachukujosh2000@gmail.com>
2023-06-08 01:51:36 -0700

    fix problem

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import "./loginform.css"
 
 const LoginForm = () => {
+
+    const [popupStyle, showPopup] = useState("hide")
+
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+    
     return (
         <div className='cover'>
             <h1>LOGIN</h1>
@@ -11,13 +19,19 @@ const LoginForm = () => {
 
 
 
-        <div className='login-btn'>Login</div>
+        <div className='login-btn' onClick={popup}>Login</div>
 
         <p className='text'>or login using</p>
         
         <div className='alt-login'>
             <div className='facebook'></div>
             <div className='gmail'></div>
+        </div>
+
+        <div className={popupStyle}>
+            <h3>Login Failed</h3>
+            <p>Username or password incorrect</p>
+
         </div>
 
         </div>
